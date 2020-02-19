f5ec57add929125581408b77bee4af1cc6440dcb
Jean-Paul <jp86mitter@gmail.com>
2020-02-18 18:33:08 -0600

    connected the js with the html using Jinja

---
+++ static/app.js
@@ -0,0 +1,29 @@
+console.log("testing if I have connection. this should display on console for index.html and comparsion.html")
+
+
+
+// function selectInit() {
+//     // Importing data from local file
+//     d3.json("/nutrientvalue").then((importData) => {
+//         console.log(importData);
+
+//         // // Searching names through json file
+//         // var names = data["names"];
+
+//         // // Adding list of id names to dropdown menu
+//         // names.map(function (idList) {
+//         //     var option = d3.select("#selDataset")
+//         //     option.append("option")
+//         //         .text(idList)
+//         //         .property("value", idList);
+//         // });
+//         // // Display default data 940
+//         // var firstData = names[0];
+
+//         // // Caling functions to display data to default id
+//         // metaData(firstData);
+//         // bar(firstData);
+//         // bubble(firstData);
+//         // gauge(firstData);
+//     });
+// };
