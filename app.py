831d2e01d578e3289fac20c60bf967def71c049e
Jean-Paul <jp86mitter@gmail.com>
2020-02-20 21:49:49 -0600

    working dropdowns

--- app.py
+++ app.py
@@ -24,31 +24,8 @@ from flask import Flask, jsonify, render_template
 connection_string = f"{pg_user}:{pg_password}@localhost:5432/{db_name}"
 engine = create_engine(f'postgresql://{connection_string}')
 
+# checking the table names
 engine.table_names()
-
-# reflect an existing database into a new model
-Base = automap_base()
-Base.prepare(engine, reflect=True)
-
-# Save reference to the tables
-NutrientValue = Base.classes.nutrientvalue
-PortionsAndWeight = Base.classes.portionsandweights
-Category = Base.classes.category
-DescriptionCategory = Base.classes.descriptioncategory
-DailyValue = Base.classes.dailyvalue
-
-# Open a communication session with the database
-session = Session(engine)
-
-# Query all tables
-nutrientValues = session.query(NutrientValue).all()
-portionsAndWeights = session.query(PortionsAndWeight).all()
-categories = session.query(Category).all()
-descriptionCategories = session.query(DescriptionCategory).all()
-dailyValues = session.query(DailyValue).all()
-
-# close the session to end the communication with the database
-session.close()
 
 
 #################################################
@@ -65,6 +42,7 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+
 @app.route("/comparison")
 def comparison():
     return render_template("comparison.html")
@@ -72,7 +50,13 @@ def comparison():
 
 @app.route('/nutrientvalue')
 def nutrientvalue():
-    df = pdsql.read_sql("SELECT * FROM nutrientvalue", engine)
+    sqlStatement = """
+    SELECT food_code, main_food_description FROM nutrientvalue
+    
+    WHERE food_code BETWEEN 11000000 AND 13120120
+    ORDER BY main_food_description;
+    """
+    df = pdsql.read_sql(sqlStatement, engine)
     df.set_index('food_code', inplace=True)
     df = df.to_json(orient='table')
 
@@ -81,21 +65,41 @@ def nutrientvalue():
 
 @app.route('/portionsandweights')
 def portionsandweights():
-
-    df = pdsql.read_sql("SELECT * FROM portionsandweights", engine)
+    sqlStatement = """
+    SELECT n.food_code, n.wweia_category_description, p.portion_description, p.portion_weight_g, n.energy_kcal, n.protein_g, n.carbohydrate_g, n.sugars_total_g, n.fiber_total_dietary_g, n.total_fat_g, n.fatty_acids_total_saturated_g, n.fatty_acids_total_monounsaturated_g, n.fatty_acids_total_polyunsaturated_g, n.cholesterol_mg, n.retinol_mcg, n.vitamin_a_rae_mcg_rae, n.carotene_alpha_mcg, n.carotene_beta_mcg, n.cryptoxanthin_beta_mcg, n.lycopene_mcg, n.lutein__zeaxanthin_mcg, n.thiamin_mg, n.riboflavin_mg, n.niacin_mg, n.vitamin_b6_mg, n.folic_acid_mcg, n.folate_food_mcg, n.folate_dfe_mcg_dfe, n.folate_total_mcg, n.choline_total_mg, n.vitamin_b12_mcg, n.vitamin_b12_added_mcg, n.vitamin_c_mg, n.vitamin_d_d2__d3_mcg, n.vitamin_e_alphatocopherol_mg, n.vitamin_e_added_mg, n.vitamin_k_phylloquinone_mcg, n.calcium_mg, n.phosphorus_mg, n.magnesium_mg, n.iron_mg, n.zinc_mg, n.copper_mg, n.selenium_mcg, n.potassium_mg, n.sodium_mg, n.caffeine_mg, n.theobromine_mg, n.alcohol_g, n.water_g
+    FROM nutrientvalue AS n 
+    INNER JOIN portionsandweights AS p
+    ON n.food_code = p.food_code
+    
+    WHERE n.food_code BETWEEN 11000000 AND 13120120
+    ORDER BY wweia_category_description;
+    """
+    df = pdsql.read_sql(sqlStatement, engine)
     df.set_index('food_code', inplace=True)
     df = df.to_json(orient='table')
 
     return df
 
+
 @app.route('/descriptioncategory')
 def descriptioncategory():
+    sqlStatment = """
+    SELECT d.category_num, c.category, d.wweia_category_code, p.wweia_category_description, p.portion_description, p.portion_weight_g, n.energy_kcal, n.protein_g, n.sugars_total_g, n.total_fat_g
+    FROM nutrientvalue AS n
+    INNER JOIN portionsandweights AS p
+    ON n.food_code = p.food_code
+    INNER JOIN descriptioncategory AS d
+    ON n.wweia_category_code = d.wweia_category_code
+    INNER JOIN category AS c
+    ON d.category_num = c.category_num
+    """
 
-    df = pdsql.read_sql("SELECT * FROM descriptioncategory", engine)
-    df.set_index('wweia_category_code', inplace=True)
+    df = pdsql.read_sql(sqlStatment, engine)
+    df.set_index('category', inplace=True)
     df = df.to_json(orient='table')
 
     return df
+
 
 @app.route('/category')
 def category():
@@ -106,6 +110,7 @@ def category():
 
     return df
 
+
 @app.route('/dailyvalue')
 def dailyvalue():
 
@@ -114,7 +119,6 @@ def dailyvalue():
     df = df.to_json(orient='table')
 
     return df
-
 
 
 if __name__ == '__main__':
--- static/app.js
+++ static/app.js
@@ -1,29 +1,70 @@
-console.log("testing if I have connection. this should display on console for index.html and comparsion.html")
+function selectInit() {
+    // Importing data from nutrientvalue
+    d3.json("/nutrientvalue").then((importData) => {
+        //console.log(importData);
+
+        // Searching food names through json
+        var names = importData.data;
+        console.log(names)
+
+        // selecting tag
+        var select = document.getElementById("selDataset");
+
+        // Dropdown list
+        for (var i = 0; i < names.length; i++) {
+            console.log("Loading dropdown. Give it time!")
+            select.innerHTML = select.innerHTML +
+                '<option value="' + names[i].food_code + '">' + names[i].main_food_description + '</option>';
+        }
+
+        // default selection
+        var defSelection = names[0].food_code
+
+        // adding default selection into selection quantity to
+        optionFoodChanged(defSelection);
+
+    });
+};
+
+function optionFoodChanged(select) {
+
+    var sel = select
+    // Importing data from portionsandweights
+    d3.json("/portionsandweights").then((importData) => {
+
+        // Searching food names through json
+        var portions = importData.data;
+        
+        //Empty Variable
+        var filterSelect = []
+
+        // Filter the search
+        filterSelect = portions.filter(d => d.food_code == sel);
+        console.log("filter:", filterSelect)
+
+        // selecting tag
+        var select = document.getElementById("selDatasetone");
+
+        //Clear 
+        select.innerHTML = "";
+
+        // For loop to create 
+        for (var i = 0; i < filterSelect.length; i++) {
+            if (filterSelect[i].portion_description != 'Quantity not specified') {
+                select.innerHTML = select.innerHTML +
+                    '<option value="' + filterSelect[i].portion_description + '">' + filterSelect[i].portion_description + '</option>';
+            }
+
+        }
+
+    });
 
 
+};
 
-// function selectInit() {
-//     // Importing data from local file
-//     d3.json("/nutrientvalue").then((importData) => {
-//         console.log(importData);
+function optionWeightChanged(select) {
 
-//         // // Searching names through json file
-//         // var names = data["names"];
 
-//         // // Adding list of id names to dropdown menu
-//         // names.map(function (idList) {
-//         //     var option = d3.select("#selDataset")
-//         //     option.append("option")
-//         //         .text(idList)
-//         //         .property("value", idList);
-//         // });
-//         // // Display default data 940
-//         // var firstData = names[0];
+};
 
-//         // // Caling functions to display data to default id
-//         // metaData(firstData);
-//         // bar(firstData);
-//         // bubble(firstData);
-//         // gauge(firstData);
-//     });
-// };
+selectInit();
